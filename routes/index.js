fd8c9785f98f4f1292c2240208f3bba1c42f3e4d
Hou <5466315@qq.com>
2016-03-28 16:09:19 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,29 @@ var webinfo = require('../my_modules/webinfo');
 
 router.get('/', function (req, res, next) {
 
-    var renderData = {};
-    webinfo.then(function (result) {
-        renderData.webinfo = result;
+    var webinfoPromise = new Promise(function (resolve, reject) {
+        webinfo.then(function (result) {
+            resolve(result)
+        })
+    });
+    var articleList = new Promise(function (resolve, reject) {
         article
             .find({})
             .skip(0)
             .limit(3)
             .populate('category')
             .exec(function (err, result) {
-                renderData.listData = result;
-                console.log(renderData);
-                res.render('index', renderData);
+                resolve(result);
             });
+    })
+    Promise.all([webinfoPromise, articleList]).then(function (value) {
+        var renderData = {
+            webinfo: value[0],
+            listData: value[1]
+        }
+        res.render('index', renderData);
+    }, function (reason) {
+        res.render('error', reason);
     });
 
 });
