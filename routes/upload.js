eb1ff19b1dfa4c44403b6a33a3646eb791889643
Hou <5466315@qq.com>
2016-06-29 14:45:15 +0800

    update

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var resJson = require('../config/response');
 var multer = require('multer');
 var upload = multer({dest: __dirname + '/../public/upload/'});
 
 router.post('/', upload.single('file'), function (req, res, next) {
-    console.log(req.file);
     res.json({
         filename: req.file.filename
     })
