c067ca11c7f364fee07fd6a06f8a4e87fbaa467d
hou <5466315@qq.com>
2017-02-08 13:42:22 +0800

    database config

--- my_modules/database.js
+++ my_modules/database.js
@@ -2,6 +2,7 @@
  * Created by Laggo on 16/1/26.
  */
 var mongoose = require("mongoose");
-var db = mongoose.createConnection('www.41js.com','blog');
+var opts = {user: 'Hou', pass: 'Hou6761886' }
+var db = mongoose.createConnection('www.41js.com','blog',opts);
 console.log('数据库连接成功')
 module.exports = db;
