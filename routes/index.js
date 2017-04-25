1b7df5506196c105bb1a9c8dce58daa64788a1a7
后居上 <test@123.com>
2017-04-25 16:52:35 +0800

    11

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var https=require('https');
 router.get('/wechat',function(req, res, next){
     var tmpUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd8d690d063af04ea&secret=eb9e8a95c6b9c57152d8a566311d3bd5&code=${req.query.code}&grant_type=authorization_code`;
     console.log(tmpUrl);
-    https.get(tmpUrl, function(req,res){
+    https.get(tmpUrl, function(req,res2){
         var html='';
         req.on('data',function(data){
             res.render('wechat', {data: data});
